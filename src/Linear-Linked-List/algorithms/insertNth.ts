409cf821c8b550ba4db9dfa9b3b8d2d63d7faae3
Reinny <reynsth@gmail.com>
2021-06-28 10:35:27 -0400

    Linear Linked List: Insert Nth

---
+++ src/Linear-Linked-List/algorithms/insertNth.ts
@@ -0,0 +1,47 @@
+/*
+	Inserts a new node at any index within a list, if the index
+	is out of bounds, add the node to the tail.
+
+	IN  -> (head: Node | null, value: number, index: number)
+	OUT -> (head: Node | null)
+*/
+
+import Node from "../node";
+
+const insertNth = (head: Node | null, value: number, index: number): Node | null => {
+	// If the head is null return null
+	// If the index is 0, swap the head and add the node in that index
+
+	if (head === null) {
+		return null;
+	};
+
+	if (index === 0) {
+		const node: Node = { value, next: head };
+		return node;
+	};
+
+	let currentNode: Node | null = head;
+	let currentIndex = 0;
+
+	while(currentNode !== null) {
+		if (currentNode.next === null) {
+			const node: Node = { value, next: null };
+			currentNode.next = node;
+			return head;
+		};
+
+		if (currentIndex === index) {
+			const node: Node = { value, next: currentNode.next };
+			currentNode.next = node;
+			return head;
+		};
+
+		currentIndex++;
+		currentNode = currentNode.next;
+	};
+
+	return head;
+};
+
+export default insertNth;
---
+++ src/Linear-Linked-List/tests/algorithms/insertNth.test.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import Node from "../../node";
+import LinearLinkedList from "../../index";
+import insertNth from "../../algorithms/insertNth";
+
+describe("Liner Linked List: InsertNth", () => {
+	const linearLinkedList = new LinearLinkedList();
+	linearLinkedList.push(1);
+	linearLinkedList.push(2);
+	linearLinkedList.push(3);
+
+	it("Inserts a new node at index 0", () => {
+		linearLinkedList.head = insertNth(linearLinkedList.head, 0, 0);
+		const values = linearLinkedList.print();
+
+		expect(values).to.have.members([0, 1, 2, 3]);
+		expect(values.length).to.equal(4);
+	});
+
+	it("Handles null head", () => {
+		const emptyLinearLinkedList = new LinearLinkedList();
+		emptyLinearLinkedList.head = insertNth(emptyLinearLinkedList.head, 0, 4);
+
+		expect(emptyLinearLinkedList.head).to.be.equal(null);
+	});
+
+	it("Inserts a node to the desired index", () => {
+		linearLinkedList.head = insertNth(linearLinkedList.head, 5, 1);
+		linearLinkedList.head = insertNth(linearLinkedList.head, 9, 2);
+
+		expect(linearLinkedList.print()).to.have.members([0, 1, 9, 5, 2, 3]);
+	});
+
+	it("Inserts a node to the tail when index is out of bounds", () => {
+		linearLinkedList.head = insertNth(linearLinkedList.head, 12, 500);
+		linearLinkedList.head = insertNth(linearLinkedList.head, 13, 100);
+		const values = linearLinkedList.print();
+
+		expect(values).to.have.members([0, 1, 9, 5, 2, 3, 12, 13]);
+		expect(values.length).to.equal(8);
+	});
+});
